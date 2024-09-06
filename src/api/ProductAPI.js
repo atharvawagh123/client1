472f4b63e5d2c85f056f303cc4df784a47428cf0
atharva <watharva383@gmail.com>
2024-09-06 17:23:51 +0530

    se

--- src/api/ProductAPI.js
+++ src/api/ProductAPI.js
@@ -6,7 +6,7 @@ const ProductAPI = () => {
     const [products,setProducts]=useState([])
 
     const getProducts = async()=> {
-        const res = await axios.get('/api/products')
+      const res = await axios.get('https://server1-yt76.onrender.com/api/products')
         setProducts(res.data.products)
     }
 
